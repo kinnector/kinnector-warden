7d55446828e3446331373a03eea49eea136347e7
goriyasno <kinnector@gmail.com>
2026-07-06 08:50:33 +0500

    Configure static linking for kinnector-core and static runtime compilation for JVMTI agent

--- build.rs
+++ build.rs
@@ -2,11 +2,13 @@ fn main() {
     // Tell cargo where to find the compiled C++ shared library
     println!("cargo:rustc-link-search=native=/home/user/Documents/kinnector/kinnector-core/build/lib");
     
-    // Link against the kinnector-core library
-    println!("cargo:rustc-link-lib=dylib=kinnector-core");
+    // Link against the static archive of kinnector-core
+    println!("cargo:rustc-link-lib=static=kinnector-core");
+    println!("cargo:rustc-link-lib=dylib=stdc++");
+    println!("cargo:rustc-link-lib=dylib=bpf");
 
     // Rebuild if the library changes
-    println!("cargo:rerun-if-changed=/home/user/Documents/kinnector/kinnector-core/build/lib/libkinnector-core.so");
+    println!("cargo:rerun-if-changed=/home/user/Documents/kinnector/kinnector-core/build/lib/libkinnector-core.a");
 
     // Compile gRPC proto
     tonic_build::compile_protos("proto/warden.proto").unwrap();
